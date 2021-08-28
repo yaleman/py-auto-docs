af93606052ffdc3ca6e6122d58ae66e1cbe194e2
James Hodgkinson <james@terminaloutcomes.com>
2021-08-28 19:34:05 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# py-auto-docs
+
+Example module for automatically documenting a python module using [mkdocs](https://www.mkdocs.org/) and [mkdocstrings](https://mkdocstrings.github.io/).
+
+Check out the source files here: https://github.com/yaleman/py-auto-docs/
---
+++ example/__init__.py
@@ -0,0 +1,14 @@
+""" An example project. """
+
+class Example():
+    """ An example class """
+
+    def __init__(self, username:str, password:str):
+        """ Stores the data. """
+
+        self.username = username
+        self.password = password
+
+    def login(self):
+        """ Logs in to the system. """
+        pass
